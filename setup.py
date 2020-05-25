b7f3fdf08bd75e8eda44ea7705fcc10114709e2f
Dawid Dabkowski <dav.dabkowski@gmail.com>
2020-05-25 17:50:12 +0100

    packaging setup updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Helpful functions for Data Science",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/DawidDabkowski/dsfun",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
